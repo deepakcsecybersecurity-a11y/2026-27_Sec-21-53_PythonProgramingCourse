0467db698f66077f4d78e4ff1abb6c3e3877baca
deepakcsecybersecurity-a11y <deepakcsecyber.security@gmail.com>
2026-08-31 16:05:11 +0530

    Arthematic_operators-Exercise

---
+++ Arthematic_Comparision_Exercise.py
@@ -0,0 +1,24 @@
+#Arthematic_operators
+a = 10
+b = 5
+
+#Addition operator
+print("Sum of a and b is:", a + b) 
+
+# Subtraction operator
+print("Difference of a and b is:", a - b) 
+
+# Multiplication operator
+print("Product of a and b is:", a * b) 
+
+# Division operator
+print("Quotient of a and b is:", a / b)  
+
+# Modulus operator
+print("Remainder of a and b is:", a % b) 
+
+# Floor Division operator
+print("Floor division of a and b is:", a // b)     
+
+# Exponentiation_operator
+print("a raised to the power of b is:", a ** b)
