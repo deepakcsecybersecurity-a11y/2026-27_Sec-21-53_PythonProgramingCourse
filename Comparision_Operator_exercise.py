37aaab37a975d4769be213903ac065265d4906da
deepakcsecybersecurity-a11y <deepakcsecyber.security@gmail.com>
2026-08-31 15:56:56 +0530

    Comparision_Operators_Execise

---
+++ Comparision_Operator_exercise.py
@@ -0,0 +1,20 @@
+#Python Comparision Operator Exercise
+a=3
+b=5
+result = a > b # Greater than operator
+print("result of ",a, ">",b,"is : ", result )
+
+result = a < b #less than operator
+print("result of",a, "<",b, "is : ", result)
+
+result = a == b #Equal to comparision
+print("result of",a, "==",b, "is : ", result)
+
+result = a != b #Not Equal to comparision
+print("result of",a, "!=",b, "is : ", result)
+
+result = a >= b #Greater than or equal to comparision
+print("result of",a, ">=",b, "is : ", result)
+
+result = a <= b #Less than or equal to comparision
+print("result of",a, "<=",b, "is : ", result)
