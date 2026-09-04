c3480d2f4306f73fffe18ff9b35599d82439e977
deepakcsecybersecurity-a11y <deepakcsecyber.security@gmail.com>
2026-09-04 13:43:00 +0530

    Identity_Operator-Exercise

---
+++ Identity_Operators_Exercise.py
@@ -0,0 +1,10 @@
+#Identity Operator Exercise
+a=[1,2,3,42,424,53]
+b=a
+result=b is a # is identity operator
+print("result of",b, "is",a,"is :",result)
+
+c=[32,543,76]
+d=[32,543,76]
+result=d is c #is identity operator
+print("result of",d,"is",c,"is :",result)
