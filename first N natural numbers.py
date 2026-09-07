317b7097a16092d2078783b61780ca346274b560
deepakcsecybersecurity-a11y <deepakcsecyber.security@gmail.com>
2026-09-07 13:34:37 +0530

    N natural numbers

---
+++ first N natural numbers.py
@@ -0,0 +1,7 @@
+# Program to calculate sum of first N naturakl numbers
+#taking input form user
+n=int(input("Enter thr number: "))
+#using the formula
+S=(n*(n+1))/2 #S is the sum of the N natural numbers
+#printing the output
+print(S)
