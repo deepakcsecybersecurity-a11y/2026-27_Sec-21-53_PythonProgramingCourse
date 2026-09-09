0db42bdd370db36d295cee29c33322f162b448f2
deepakcsecybersecurity-a11y <deepakcsecyber.security@gmail.com>
2026-09-09 11:50:10 +0530

    Update calculate compound interest.py
    added some comments in the code

--- calculate compound interest.py
+++ calculate compound interest.py
@@ -1,9 +1,9 @@
  # Program to calculate Compound Interest
-
+#Taking inputs from the user
 P = float(input("Enter the principal amount: "))
 R = float(input("Enter the rate of interest: "))
 T = float(input("Enter the time in years: "))
-
+#computing Compound interest using the below formula
 CI = P * (1 + R / 100) ** T - P
-
+#printing the output(Compound interest)
 print("Compound Interest =", CI)
