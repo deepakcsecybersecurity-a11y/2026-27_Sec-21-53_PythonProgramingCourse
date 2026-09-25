ea1ce2f05351e04a7d045b15b7a46cab94465818
deepakcsecybersecurity-a11y <deepakcsecyber.security@gmail.com>
2026-09-25 15:04:26 +0530

    Add files via upload

---
+++ Unit-2A_Task-3_Positive_Negative_Zero.py
@@ -0,0 +1,7 @@
+Number = int(input("Enter a random numer: "))
+if Number > 0:
+    print(f"The number {Number} is a Positive number")
+elif Number < 0:
+    print(f"The number {Number} is a Negative number")
+else:
+    print(f"The number is zero")
