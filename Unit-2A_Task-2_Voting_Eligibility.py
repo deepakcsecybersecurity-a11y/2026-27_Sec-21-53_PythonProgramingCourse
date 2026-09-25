f6d8fa47ecb5871dddaf02bae1328264f0ab164a
deepakcsecybersecurity-a11y <deepakcsecyber.security@gmail.com>
2026-09-25 14:55:07 +0530

    Add files via upload

---
+++ Unit-2A_Task-2_Voting_Eligibility.py
@@ -0,0 +1,5 @@
+age_in_years = int(input("Enter your age: "))
+if age_in_years < 18:
+    print(f"people with Age group of below {age_in_years} years: Not eligible for voteing")
+else:
+    print(f"People with Age group of {age_in_years} years or even more is: Eligible for voting")
