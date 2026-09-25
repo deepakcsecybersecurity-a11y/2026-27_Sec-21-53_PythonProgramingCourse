d2b6de70bf57ae9aa7afb6ecff6ee1130ac18c9c
deepakcsecybersecurity-a11y <deepakcsecyber.security@gmail.com>
2026-09-25 14:23:03 +0530

    Add files via upload

---
+++ Unit-1_Task-15_Type_Conversion.py
@@ -0,0 +1,35 @@
+#int to float
+int_num=10
+float_num=float(int_num)
+print(float_num)
+print(type(float_num))
+
+#int to str
+int_num=10
+str_num=str(int_num)
+print(str_num)
+print(type(str_num))
+
+#float to int
+float_num=10.34
+int_num=int(float_num)
+print(int_num)
+print(type(int_num))
+
+#float to str
+float_num=10.389465
+str_num=str(float_num)
+print(str_num)
+print(type(str_num))
+
+#str to int
+str_num="10"
+int_num=int(str_num)
+print(int_num)
+print(type(int_num))
+
+#str to float
+str="105.76456"
+float_num=float(str_num)
+print(float_num)
+print(type(float_num))
